22aae57d18a18a41f7937ffc6b353e961131d342
Maxio99 <91377824+Maxio99@users.noreply.github.com>
2023-09-08 00:05:45 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,46 @@
+import os
+#import torch
+#import argparse
+import matplotlib.pyplot as plt
+from flask import Flask, send_file,request
+from coloriztaion/colorizers import *
+from colorizaiton/demo_release import load_img, preprocess_img, postprocess_tens
+
+app = Flask(__name__)
+
+# Load colorizers
+colorizer_eccv16 = eccv16(pretrained=True).eval()
+colorizer_siggraph17 = siggraph17(pretrained=True).eval()
+
+@app.route('/', methods=['GET','POST'])
+def colorize():
+    try:
+        # Set the image path
+        #image_path = r'C:\Users\gaith\Desktop\colorization\imgs\happy_dog.jpg'
+        image_path = request.files['image']
+        print(f"request: ", {image_path})
+    
+    
+        # Load and preprocess the image
+        img = load_img(image_path)
+        (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256, 256))
+    
+        # Colorize the image
+        out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs))
+        out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs))
+    
+        # Save the colorized image to the desktop
+        # desktop = os.path.expanduser("~/Desktop")
+        # output_path = os.path.join(desktop, "colorized_image.png")
+        # plt.imsave(output_path, out_img_siggraph17)
+    
+        # Send the colorized image as a response
+    
+        return send_file(output_path, mimetype='image/png')
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return "failed"
+
+
+if __name__ == '__main__':
+    app.run()
