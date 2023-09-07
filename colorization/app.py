3a9568107a99b7481f37d176dcb0724752a87d00
GhaithAshkar <140319226+GhaithAshkar@users.noreply.github.com>
2023-09-07 21:22:27 +0300

    Update app.py

--- colorization/app.py
+++ colorization/app.py
@@ -14,28 +14,33 @@ colorizer_siggraph17 = siggraph17(pretrained=True).eval()
 
 @app.route('/', methods=['GET','POST'])
 def colorize():
-    # Set the image path
-    #image_path = r'C:\Users\gaith\Desktop\colorization\imgs\happy_dog.jpg'
-    image_path = request.files['image']
-    print(f"request: ", {image_path})
+    try:
+        # Set the image path
+        #image_path = r'C:\Users\gaith\Desktop\colorization\imgs\happy_dog.jpg'
+        image_path = request.files['image']
+        print(f"request: ", {image_path})
+    
+    
+        # Load and preprocess the image
+        img = load_img(image_path)
+        (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256, 256))
+    
+        # Colorize the image
+        out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs))
+        out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs))
+    
+        # Save the colorized image to the desktop
+        # desktop = os.path.expanduser("~/Desktop")
+        # output_path = os.path.join(desktop, "colorized_image.png")
+        # plt.imsave(output_path, out_img_siggraph17)
+    
+        # Send the colorized image as a response
+    
+        return send_file(output_path, mimetype='image/png')
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return "failed"
 
-
-    # Load and preprocess the image
-    img = load_img(image_path)
-    (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256, 256))
-
-    # Colorize the image
-    out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs))
-    out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs))
-
-    # Save the colorized image to the desktop
-    desktop = os.path.expanduser("~/Desktop")
-    output_path = os.path.join(desktop, "colorized_image.png")
-    plt.imsave(output_path, out_img_siggraph17)
-
-    # Send the colorized image as a response
-
-    return send_file(output_path, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run()
